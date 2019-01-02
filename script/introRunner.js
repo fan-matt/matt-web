5a994390c5cc3c9449b7c95c8eadb51695b02bc9
Matthew Fan <fn.matthew@outlook.com>
2019-01-02 12:00:42 -0600

    cleanup fade script

--- script/introRunner.js
+++ script/introRunner.js
@@ -2,11 +2,14 @@ $(document).ready(run);
 
 
 function run() {
-    $(".in-out-fade").hide(0).delay(1000).fadeToggle(500);
+    const FADE_SPEED = 250;
+    const SHOW_TIME = 1500;
+
+    $(".in-out-fade").hide(0).delay(1000).fadeToggle(FADE_SPEED);
 
     let counter = 0;
 
-    setTimeout(fadeOutIn , 2000);
+    setTimeout(fadeOutIn , SHOW_TIME + 2 * FADE_SPEED);
 
     function fadeOutIn() {
         let word;  
@@ -20,15 +23,18 @@ function run() {
                 word = "Dog Lover";
                 break;
 
+            case 2:
+                word = "Problem Solver";
+                break;
+
             default:
                 return;
         }
         counter++;
 
-        $(".in-out-fade").fadeToggle(500);
-        $(".in-out-fade").fadeToggle(500);
-        window.setTimeout(function() {$(".in-out-fade").text(word);} , 500)
+        $(".in-out-fade").fadeToggle(FADE_SPEED).fadeToggle(FADE_SPEED);
+        window.setTimeout(function() {$(".in-out-fade").text(word);} , FADE_SPEED)
 
-        setTimeout(fadeOutIn , 2000);
+        setTimeout(fadeOutIn , SHOW_TIME);
     }
 }
