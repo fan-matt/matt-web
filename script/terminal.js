cced8699300c92e2bc6d6fd93c64bab060fec8e2
Matthew Fan <fn.matthew@outlook.com>
2019-06-19 01:58:21 -0500

    Cookie to track Last Login

--- script/terminal.js
+++ script/terminal.js
@@ -1,3 +1,14 @@
+function getLastLoginString() {
+    let rawDate = new Date();
+    let year = String(rawDate.getFullYear());
+    let time = rawDate.toTimeString();
+    time = time.substring(0 , time.indexOf(" "));
+    let date = rawDate.toString();
+    date = date.substring(0 , date.indexOf(year));
+    return date + time + " " + year;
+}
+
+
 document.addEventListener('DOMContentLoaded', function() {
     let cursorVisible = true;
 
@@ -13,5 +24,23 @@ document.addEventListener('DOMContentLoaded', function() {
 
         cursorVisible = !cursorVisible;
     } , 750);
+
+
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let storedDate = decodedCookie.split(";")[0];
+    storedDate = storedDate.substring(10);
+
+    console.log(decodedCookie);
+    console.log(storedDate);
+    if(storedDate == "") {
+        document.getElementsByClassName("last-login")[0].textContent = getLastLoginString();
+    }
+    else {
+        document.getElementsByClassName("last-login")[0].textContent = storedDate;
+    }
+
+    let now = new Date();
+    now.setFullYear(now.getFullYear() + 10);
+    document.cookie = "lastLogin=" + escape(getLastLoginString()) + ";expires=" + now.toUTCString();
 
 }, false);
